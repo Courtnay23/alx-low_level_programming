36257cb0561bd0ffdf3cb5f2038b5cbb7d1d56f9
Courtnay <courtnaydavids2@gmail.com>
2022-06-11 03:48:40 -0700

    test on long

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
     char d;
     float f;
     
-    printf("Size of a char: %lu byte(s)\n", (unsigned char)sizeof(d));
+    printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
     printf("Size of an int: %lu byte(s)\n", (unsigned int)sizeof(a));
     printf("Size of a long int: %lu byte(s)\n", (unsigned long int)sizeof(b));
     printf("Size of a long long int: %lu byte(s)\n", (unsigned long long int)sizeof(c));
