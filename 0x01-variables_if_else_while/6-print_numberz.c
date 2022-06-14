90084a46899a4adc29940bc132a4e341a0511334
Courtnay <courtnaydavids2@gmail.com>
2022-06-14 14:03:05 -0700

    Simple when you think about it

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main = Entry Point
+ * main - Entry Point
  *
- * Return = 0 (SUCCESS)
+ * Return: 0 (SUCCESS)
  */
 int main(void)
 {
-return(0);
+char x;
+for (x = '0'; x <= '9'; x++)
+putchar(x);
+putchar('\n');
+return (0);
 }
