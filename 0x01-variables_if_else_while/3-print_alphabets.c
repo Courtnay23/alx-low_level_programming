4be4b82d515a7cea6fd2e44a0656f5837798d777
Courtnay <courtnaydavids2@gmail.com>
2022-06-14 13:27:44 -0700

    Test the typing skills

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,8 +8,11 @@
 int main(void)
 {
 char x;
-for (x = 'a'; x <= 'Z'; x++)
+char y;
+for (x = 'a'; x <= 'z'; x++)
 putchar(x);
+for (y = 'A'; y <= 'Z'; y++)
+putchar(y);
 putchar('\n');
 return (0);
 }
