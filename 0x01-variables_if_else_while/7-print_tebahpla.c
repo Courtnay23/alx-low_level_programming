6bd95cb1ae64c190dc57b987617acb34ad2d7a93
Courtnay <courtnaydavids2@gmail.com>
2022-06-14 14:07:45 -0700

    Test of small change

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main = Entry Point
+ * main - Entry Point
  *
- * Return = 0 (SUCCESS)
+ * Return: 0 (SUCCESS)
  */
 int main(void)
 {
-return(0);
+char x;
+for (x = 'z'; x <= 'a'; x++)
+putchar(x);
+putchar('\n');
+return (0);
 }
