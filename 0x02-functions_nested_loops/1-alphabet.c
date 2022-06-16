a50d457678da11aea72c82c30daf14c68b54ed95
Courtnay <courtnaydavids2@gmail.com>
2022-06-16 09:13:50 -0700

    Test the layout

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,9 +2,9 @@
 /**
  * main - Check description
  * Description: It prints the alphabet in lowercase, followed by a new line.
- * Return: 0 (SUCCESS)
+ * Return: nothing
  */
-int main(void)
+void print_alphabet(void)
 {
 char x;
 for (x = 'a'; x <= 'z'; x++)
