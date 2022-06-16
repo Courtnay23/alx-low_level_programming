ea05a3f430caa5a760d8ffa8cee0ef21e381d502
Courtnay <courtnaydavids2@gmail.com>
2022-06-16 07:18:52 -0700

    Test for 80

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,15 +8,15 @@
 int main(void)
 {
 int x, y;
-for (x = '0'; x <= '98'; x++)
+for (x = '0'; x <= "98"; x++)
 {
-for (y = x + 1; y <= '98'; y++)
+for (y = x + 1; y <= "98"; y++)
 {
 putchar((x / 10) + '0');
 putchar((x % 10) + '0');
 putchar(' ');
-putchar((q / 10) + '0');
-puthcar((q % 10) + '0');
+putchar((y / 10) + '0');
+puthcar((y % 10) + '0');
 if (x == '98' && y == '99')
 continue;
 putchar(',');
