46518a70d89fb8a49e92547d25b3dac1b9838792
Courtnay <courtnaydavids2@gmail.com>
2022-06-16 06:57:32 -0700

    Test for 80

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (SUCCESS)
+ */
+int main(void)
+{
+int x, y;
+for (x = '0'; x <= '9'; x++)
+{
+for (y = d + 1; y <= '9'; y++)
+if (y != x)
+{
+putchar(x);
+putchar(y);
+if (x == '8' && y == '9')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
