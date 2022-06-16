a290a0677524f8b17258120b25b825b6d046d3f3
Courtnay <courtnaydavids2@gmail.com>
2022-06-16 08:26:55 -0700

    Test this

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main = Entry Point
- *
- * Return = 0 (SUCCESS)
+ * main - Check description
+ * Description: It prints the word Holberton, followed by a new line.
+ * Return: 0 (SUCCESS)
  */
 int main(void)
 {
-return(0);
+char word[9]= "Holberton";
+int x;
+for (x = 0; x < 9; x++)
+_putchar(word[x]);
+_putchar('\n');
+return (0);
 }
