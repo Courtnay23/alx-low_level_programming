3d87014967e90f1b5485bfc193f2557b7e39893a
Courtnay <courtnaydavids2@gmail.com>
2022-06-16 09:10:07 -0700

    test this thing

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,13 @@
-#include <unistd.h>
 #include "holberton.h"
 /**
  * main - Check description
  * Description: It prints the alphabet in lowercase, followed by a new line.
  * Return: 0 (SUCCESS)
  */
-int print_alphabet(void)
+int main(void)
 {
 char x;
 for (x = 'a'; x <= 'z'; x++)
 _putchar(x);
 _putchar('\n');
-return (0);
 }
