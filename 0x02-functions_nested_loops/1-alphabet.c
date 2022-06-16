2c65dfac5709d345fe4fddcc4bb5df02e5a4d35c
Courtnay <courtnaydavids2@gmail.com>
2022-06-16 09:19:23 -0700

    Test for 80

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,10 +4,11 @@
  * Description: It prints the alphabet in lowercase, followed by a new line.
  * Return: nothing
  */
-void print_alphabet(void)
+int main(void)
 {
 char x;
 for (x = 'a'; x <= 'z'; x++)
 _putchar(x);
 _putchar('\n');
+return ();
 }
