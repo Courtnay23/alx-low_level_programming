9f77fac81547cda76cd6be76c0df35df3db18c74
courtnay <courtnaydavids2@gmail.com>
2022-09-09 06:16:43 -0700

    Test if it can compile

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-    write(STDERR, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    fprintf(STDERR, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
