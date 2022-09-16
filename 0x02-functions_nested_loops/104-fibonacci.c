80603132caf27e15d0ff8a77aa5f779d206cb577
courtnay <courtnaydavids2@gmail.com>
2022-09-16 02:40:29 -0700

    Test for passing

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,13 +7,13 @@ int main(void)
 {
 int inc;
 unsigned long n1 = 0, n2 = 1, n3;
-for (inc = 0; inc < 98; inc++)
+for (inc = 0; inc < 99; inc++)
 {
 n3 = n1 + n2;
 printf("%lu", n3);
 n1 = n2;
 n2 = n3;
-if (inc == 97)
+if (inc == 98)
 printf("\n");
 else
 printf(", ");
