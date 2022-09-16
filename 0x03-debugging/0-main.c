fcb486bbd8fbad0b92cd1e1dac223d04cce57b1f
courtnay <courtnaydavids2@gmail.com>
2022-09-16 05:23:40 -0700

    Test

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
- * main - tests if integer is postive of negative
- *
+ * main - tests if integer is positive of negative
  * Return: 0
  */
+
 int main(void)
 {
 int i;
+
 i = 0;
 positive_or_negative(i);
+
 return (0);
 }
