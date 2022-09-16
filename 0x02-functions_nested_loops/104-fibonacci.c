a4cde45e8af6f0d5a780951b9ddd5f5ec1c5c31b
courtnay <courtnaydavids2@gmail.com>
2022-09-16 02:36:17 -0700

    Bending code

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+/**
+ * main - prints the first 98 fibonacci numbers
+ * Return: 0
+ */
+int main(void)
+{
+int inc;
+unsigned long n1 = 0, n2 = 1, n3;
+for (inc = 0; inc < 98; inc++)
+{
+n3 = n1 + n2;
+printf("%lu", n3);
+n1 = n2;
+n2 = n3;
+if (inc == 97)
+printf("\n");
+else
+printf(", ");
+}
+return (0);
+}
