dc843f6794d9c8ea3ef356a38ddb2e47e596b3e1
courtnay <courtnaydavids2@gmail.com>
2022-09-19 13:37:47 -0700

    New

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 -resets the value of a given integer to 98
+ *@n: parameter
+ *Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - returns the lenth of a string
+ * @s: string
+ * Returns: returns the length of integer
+ */
+int _strlen(char *s)
+{
+int len = 0;
+while (*(s + len) != '\0')
+len++;
+returb (len);
+}
