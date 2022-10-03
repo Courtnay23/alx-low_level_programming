f72b28456579b1b1a2423c28ae1181500c30faef
courtnay <courtnaydavids2@gmail.com>
2022-10-03 14:34:21 -0700

    Small changes

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - prints the number of arguments passed to it
+ *@argc: number of arguments passed to the function
+ *@argv: argument vector of pointers to string
+ *Return: always 0
+ */
+int main(int argc, char *argv[] _attribute_((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}
